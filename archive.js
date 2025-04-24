8589176242e525ca8a593842a142905f7feb3913
leviticushood <hoodlevi2@gmail.com>
2025-04-24 01:10:28 -0700

    Update archive.js

--- archive.js
+++ archive.js
@@ -1,19 +1,22 @@
 function loadArchivedEvents() {
   const events = JSON.parse(localStorage.getItem('events') || '[]');
   const today = new Date().toISOString().split('T')[0];
+  const archived = events.filter(event => event.date < today);
+
   const tbody = document.querySelector('#archiveTable tbody');
   tbody.innerHTML = '';
-  events.filter(event => event.date < today).forEach(event => {
+
+  archived.forEach(event => {
     const row = document.createElement('tr');
     row.innerHTML = `
       <td>${event.title}</td>
       <td>${event.date}</td>
       <td>${event.time}</td>
       <td>${event.location}</td>
-      <td><a href='${event.link}' target='_blank'>Link</a></td>
+      <td><a href="${event.link}" target="_blank">Link</a></td>
       <td>${event.contact}</td>
       <td>${event.description}</td>
-      <td class='user-name'>${event.user}</td>
+      <td class="user-name">${event.user}</td>
     `;
     tbody.appendChild(row);
   });
